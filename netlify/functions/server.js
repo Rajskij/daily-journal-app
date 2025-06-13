b0d9d5fadf2bc03bdfd77fb80fb0ff25427a1fad
Rajski <ihor.haidukov@gmail.com>
2025-06-13 19:18:41 -0400

    add configs

---
+++ netlify/functions/server.js
@@ -0,0 +1,31 @@
+import express, {Router} from 'express'
+import serverless from 'serverless-http'
+import { routes as home } from './routes/home.js';
+import { routes as user } from './routes/users.js';
+import { routes as tags } from './routes/tags.js';
+import { routes as history } from './routes/history.js';
+
+const app = express()
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+app.use('/home', home);
+app.use('/users', user);
+app.use('/tags', tags);
+app.use('/history', history);
+
+app.use((err, req, res, next) => {
+    res.status(err.status | 500);
+    res.json({ error: err.message });
+});
+
+export const handler = serverless(app)
