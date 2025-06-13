dcba11cecd7e465d4b897d574fb527eaabe71ece
Rajski <ihor.haidukov@gmail.com>
2025-06-13 12:07:04 -0400

    add PUT req for model dialog

--- public/handler/eventHandler.js
+++ public/handler/eventHandler.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const userDeleteBtn = document.querySelectorAll('.delete-btn');
     const editBtn = document.querySelectorAll('.edit-btn');
+    const editForm = document.getElementById('edit-form');
 
     userDeleteBtn.forEach(btn => {
         btn.addEventListener('click', async event => {
@@ -33,6 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
     editBtn.forEach(btn => {
         btn.addEventListener('click', event => {
             const button = event.currentTarget;
+            const id = button.getAttribute('data-id');
+            const userId = button.getAttribute('data-user-id');
             const username = button.getAttribute('data-username');
             const date = button.getAttribute('data-date');
             const note = button.getAttribute('data-note');
@@ -40,14 +43,14 @@ document.addEventListener('DOMContentLoaded', () => {
             const weather = button.getAttribute('data-weather');
             const tags = JSON.parse(button.getAttribute('data-tags'));
 
-            console.log(tags)
-            // tags.forEach(tag => console.log(tag));
             const usernameEl = document.getElementById('username');
             const userDate = document.getElementById('dateInput');
             const userNote = document.getElementById('note');
             const userMood = document.getElementById(mood);
             const userWeather = document.getElementById(weather);
 
+            usernameEl.setAttribute('entryId', id);
+            usernameEl.setAttribute('userId', userId);
             usernameEl.innerText = `User: ${username}`;
             userDate.value = date;
             userNote.value = note;
@@ -60,4 +63,53 @@ document.addEventListener('DOMContentLoaded', () => {
             tags.forEach(tag => document.getElementById(tag).checked = true);
         });
     })
+
+    editForm.addEventListener('submit', async function (event) {
+        event.preventDefault();
+        console.log("-- Form submission started --");
+
+        // Extract values
+        const user = document.getElementById('username');
+        const mood = document.getElementById('moods').value;
+        const emojiMood = document.getElementById(mood).textContent;
+        const weather = document.getElementById('weather').value;
+        const date = document.getElementById('dateInput').value;
+        const note = document.getElementById('note').value;
+        console.log(document.getElementById('weather'));
+        // Extract checked tags
+        const checkedTags = Array.from(document.querySelectorAll('input[name="tags"]:checked'))
+            .map(tagCheckbox => tagCheckbox.id);
+
+        // Debug output
+        console.log('Mood:', mood);
+        console.log('Emoji Mood', emojiMood);
+        console.log('Weather:', weather);
+        console.log('Date:', date);
+        console.log('Note:', note);
+        console.log('Tags:', checkedTags);
+
+        try {
+            const response = await fetch(`/history/update`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    id: user.getAttribute('entryId'),
+                    userId: user.getAttribute('userId'),
+                    mood: emojiMood,
+                    weather,
+                    date,
+                    note,
+                    tags: checkedTags
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else {
+                window.location.reload();
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Failed to delete user');
+        }
+    });
 });
--- routes/history.js
+++ routes/history.js
@@ -9,13 +9,43 @@ const users = db.users;
 routes
     .route('/')
     .get((req, res) => {
+        const query = req.query;
+        let history = moodHistory;
+
+        if (query && query.userId) {
+            const userId = query.userId;
+            history = moodHistory.filter(val => val.userId == userId);
+        }
+
         res.render('history', {
             users,
-            history: moodHistory,
+            history,
             moods,
             weathers,
             tags: db.tags
         });
+    });
+
+routes
+    .route('/update')
+    .put((req, res) => {
+        const body = req.body;
+        const id = body.id;
+        const entry = moodHistory.find(entry => entry.id == id)
+        const index = moodHistory.indexOf(entry);
+        // console.log(entry, " new: ", body);
+
+        moodHistory[index] = {
+            id: Number(body.id),
+            userId: Number(body.userId),
+            date: body.date,
+            mood: body.mood,
+            weather: body.weather,
+            tags: body.tags,
+            note: body.note
+        };
+
+        res.redirect('/history');
     });
 
 export { routes };
